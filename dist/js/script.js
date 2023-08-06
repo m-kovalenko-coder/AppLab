8e50da0c9ef37e90ee144de04a69baffb9288cc9
Maksym Kovalenko <m.kovalenko.coder@gmail.com>
2023-08-06 20:56:45 +0300

    Added Modal box

--- dist/js/script.js
+++ dist/js/script.js
@@ -60,5 +60,36 @@ overlayClose.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+////////// MODAL //////////
+ 
+// Get the button that opens the modal
+let btn = document.getElementById("button");
+let btn2 = document.getElementById("button-s");
+
+// Get the modal
+let modal = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("modal__close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
